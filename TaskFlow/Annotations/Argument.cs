7ad3aa38470c38fb86bfdff187a5c981b43979d5
Volodymyr Dombrovskyi <dombrovsky@users.noreply.github.com>
2023-05-07 21:53:59 +0200

    Project structure

---
+++ TaskFlow/Annotations/Argument.cs
@@ -0,0 +1,31 @@
+namespace System.Threading.Tasks.Flow.Annotations
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    internal static class Argument
+    {
+        public static void NotNull([NotNull] object? argument, [CallerArgumentExpression("argument")] string? parameterName = null)
+        {
+#if NET6_0
+            ArgumentNullException.ThrowIfNull(argument, parameterName);
+#else
+            if (argument is null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+#endif
+        }
+
+        public static void Assert<T>(in T argument, Func<T, bool> predicate, string message, [CallerArgumentExpression("argument")] string? parameterName = null)
+        {
+            Argument.NotNull(predicate);
+            Argument.NotNull(message);
+
+            if (!predicate(argument))
+            {
+                throw new ArgumentException(message, parameterName);
+            }
+        }
+    }
+}
