4993e370803682f283a4fca6d25a16503ae81a84
Volodymyr Dombrovskyi <dombrovsky@users.noreply.github.com>
2023-08-26 21:12:04 +0200

    Split ServiceCollectionExtensionsFixture

---
+++ TaskFlow.Extensions.Microsoft.DependencyInjection.Tests/CustomTaskFlowServiceCollectionExtensionsFixture.cs
@@ -0,0 +1,24 @@
+namespace TaskFlow.Extensions.Microsoft.DependencyInjection.Tests
+{
+    using global::Microsoft.Extensions.DependencyInjection;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System.Threading.Tasks.Flow;
+
+    [TestFixture]
+    public sealed class CustomTaskFlowServiceCollectionExtensionsFixture
+    {
+        [Test]
+        public void AddTaskFlow_ShouldRegisterTaskFlowInfoAndTaskScheduler()
+        {
+            using var container = new ServiceCollection()
+                .AddTaskFlow(null, (provider, options) =>
+                {
+                    var taskFlow = Substitute.For<ITaskFlow>();
+                    taskFlow.Options.Returns(options);
+                    return taskFlow;
+                })
+                .BuildServiceProvider();
+        }
+    }
+}
